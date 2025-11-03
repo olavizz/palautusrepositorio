6cf92a847e8407ecc11e215e31791aa0e5ab29b7
Olavi Suojala <o.suojala@gmail.com>
2025-11-03 10:26:53 +0200

    playerlist

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,25 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    print("Players from FIN:")
+    print("")
+
+    for player_dict in response:
+        player = Player(player_dict)
+
+        if player.nationality == "FIN":
+            players.append(player)
+        else:
+            continue
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
