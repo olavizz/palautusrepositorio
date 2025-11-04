5e001e1c654cfcfa51af661c845678de4b1527f5
Olavi Suojala <o.suojala@gmail.com>
2025-11-04 09:34:11 +0200

    modified index.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,4 +1,15 @@
 from logger import logger
+from summa import summa
+from erotus import erotus
+
+logger("aloitetaan")
+
+x = int(input("luku 1: "))
+y = int(input("luku 2: "))
+print(f"{summa(x, y)}")
+print(f"{erotus(x, y)}")
+
+logger("lopetetaan")from logger import logger
 
 logger("aloitetaan")
 
